080ffe8c1793c7c7c896a205554127e9bfa219de
deedeecee <76088949+deedeecee@users.noreply.github.com>
2021-07-26 09:12:10 +0530

    Modified inverseFinder

--- Assignment-2/inverseFinder.java
+++ Assignment-2/inverseFinder.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.Math;
 public class inverseFinder {
-    public static int digitCount (int n){
+    public static int digitCount (int n){   //returns the number of digits in the integer
         int count = 0;
         while (n != 0){
             count++;
@@ -9,18 +9,16 @@ public class inverseFinder {
         }
         return count;
     }
-    public static int position (int n, int x){
-        int count = 0;
+    public static int position (int n, int x){  //returns the position of digit x in the given integer n
         while (n != 0){
             if ((n % 10) == x){
-                count++;
                 break;
             }
             else{
                 n /= 10;
             }
         }
-        return (digitCount(n) - count + 1);
+        return digitCount(n);
     }
     public static void main(String[] args){
         int num, n, res = 0;
